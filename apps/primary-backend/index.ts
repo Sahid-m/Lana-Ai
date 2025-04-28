ca1ddab28b4d0e41dd93a0f185f210ec05322fac
Sahid-m <sahidmunjavar.s@gmail.com>
2025-04-28 03:54:29 +0100

    primary-backend added

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -1,1 +1,44 @@
-console.log("Hello via Bun!");
+import { authMiddleware } from "common/middleware";
+import cors from "cors";
+import { prismaClient } from "db/client";
+import express from "express";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/project", authMiddleware, async (req, res) => {
+  const { prompt, type } = req.body;
+  const userId = req.userId!;
+  //TODO: add logic to get a useful name for the project from the prompt
+  const description = prompt.split("\n")[0];
+  const project = await prismaClient.project.create({
+    data: { description, userId, type },
+  });
+  res.json({ projectId: project.id });
+});
+
+app.get("/projects", authMiddleware, async (req, res) => {
+  const userId = req.userId!;
+  const projects = await prismaClient.project.findMany({
+    where: { userId },
+  });
+  res.json({ projects });
+});
+
+app.get("/prompts/:projectId", authMiddleware, async (req, res) => {
+  const userId = req.userId!;
+  const projectId = req.params.projectId;
+
+  const prompts = await prismaClient.prompt.findMany({
+    where: { projectId },
+    include: {
+      actions: true,
+    },
+  });
+  res.json({ prompts });
+});
+
+app.listen(9090, () => {
+  console.log("Server is running on port 9090");
+});
