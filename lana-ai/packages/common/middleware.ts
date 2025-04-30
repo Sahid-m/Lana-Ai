885bcd0b6bd05fc722c293355e24d93c681304e8
Sahid-m <sahidmunjavar.s@gmail.com>
2025-04-30 18:23:07 +0100

    changes and Dockerfile done

--- lana-ai/packages/common/middleware.ts
+++ lana-ai/packages/common/middleware.ts
@@ -4,7 +4,18 @@ import jwt from "jsonwebtoken";
 
 dotenv.config();
 
-const JWT_PUBLIC_KEY = process.env.JWT_PUBLIC_KEY;
+const JWT_PUBLIC_KEY =
+  process.env.JWT_PUBLIC_KEY ??
+  `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw50fkkTFeRms0mCLm2ww
+KLVZ8QZVBXJ024FQkImAS5PQ8yHew8asPR/n4JxscGFKgWL3GAzq+0dg6G/g4u4z
+YhBvxul5wC5YMr7UJ1Aosgnu1yCpzaLJzMB2k4E62gRFD9h3ABpvcMIVrcINN/ok
+52Kyl6grfedhrr9pN9ZFEW1St0P4xI9nirHHLbDxFRXxb9iZUUUccrevmP2zHp8e
+ACjR+qtvagWqH9GBmoHVqkYM5olYzH5Vhm4viTYo/neYT/2MK4Q3bG4hsFou8CIN
+aDwA7eooiO6pRGlsBx8BYnGe9EbRiWYovTjHXVvVMvrrIEGoPd4sky8xEi6eMGiC
+LwIDAQAB
+-----END PUBLIC KEY-----
+`;
 
 export function authMiddleware(
   req: Request,
